3f488c755af29c4d5f9cd164e55dadf5a5287350
tom <tomowatt@me.com>
2021-02-13 11:35:12 +0000

    Updating Javascript to reference new Backend URL
    * Added function to get Emojiphrase and handle any connection errors

--- docs/main.js
+++ docs/main.js
@@ -1,15 +1,34 @@
 function getBackend() {
-    return 'https://get.apassphrase.me/'
+    return 'https://api.apassphrase.me/'
 }
 
 async function getPassphrase() {
+    let backend = getBackend() + 'passphrase'
     try {
-        await fetch(getBackend()).then(response => response.json()).then(data => displayPassphrase(data.passphrase));
+        await fetch(backend).then(response => response.json()).then(data => displayPassphrase(data.passphrase));
     } catch (e) {
-        displayPassphrase("Could not connect to: " + getBackend());
+        displayPassphrase("Could not connect to: " + backend);
     }
 }
 
 function displayPassphrase(data) {
     document.getElementById("passphrase").innerHTML = data;
+}
+
+async function getEmojiphrase() {
+    let backend = getBackend() + 'emojiphrase'
+    try {
+        await fetch(backend).then(response => response.json()).then(data => displayEmojiphrase(data));
+    } catch (e) {
+        displayEmojiphrase("Could not connect to: " + backend);
+    }
+}
+
+function displayEmojiphrase(data) {
+    if (typeof data === 'string' || data instanceof String) {
+        document.getElementById("emojiphrase").innerHTML = data;
+    } else {
+        document.getElementById("emojiphrase").innerHTML = data.emojiphrase;
+        document.getElementById("emojis").innerHTML = data.emojis;
+    }
 }
